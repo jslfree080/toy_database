a9235741aa1fdf728ac60160affe4fc8498d411c
Jung Soo Lee <jslfree080@gmail.com>
2024-02-20 10:44:51 +0900

    Update db_connection.rs

--- src/db_connection.rs
+++ src/db_connection.rs
@@ -1,14 +1,20 @@
-use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use dotenvy::dotenv;
 use std::env;
+use diesel::r2d2::{ Pool, PooledConnection, ConnectionManager, PoolError };
 
-pub fn establish_connection() -> PgConnection {
-    dotenv()
-    .ok();
+pub type PgPool = Pool<ConnectionManager<PgConnection>>;
+pub type PgPooledConnection = PooledConnection<ConnectionManager<PgConnection>>;
 
-    let database_url = env::var("DATABASE_URL")
-    .expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-    .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+fn init_pool(database_url: &str) -> Result<PgPool, PoolError> {
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+
+    Pool::builder().build(manager)
+}
+
+pub fn establish_connection() -> PgPool {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    init_pool(&database_url).expect("Failed to create pool")
 }
